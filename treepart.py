c021fdf0a53bc823d1d679e4d24a4b30cf5a980c
ghst659 <gst659@gmail.com>
2020-09-20 15:23:45 -0700

    Generalised DFS traversal for printing and partitioning.

--- treepart.py
+++ treepart.py
@@ -4,7 +4,18 @@ import collections
 import os
 import sys
 
+def build(linestream, include_leaf=True):
+  """Builds a tree given from an iterable of lines."""
+  top = TreeNode()
+  for line in linestream:
+    if not line.startswith("/"):
+      continue
+    line_parts = line.strip().lstrip("/").split("/")
+    top.add_leaf(line_parts, include_leaf=include_leaf)
+  return top
+
 class TreeNode:
+  """A single node in the weighted tree."""
   def __init__(self):
     self._weight = 0
     self._children = collections.defaultdict(TreeNode)
@@ -12,10 +23,6 @@ class TreeNode:
   @property
   def weight(self):
     return self._weight
-
-  @weight.setter
-  def weight(self, new_weight):
-    self._weight = new_weight
 
   def child_names(self):
     return self._children.keys()
@@ -30,27 +37,35 @@ class TreeNode:
     if include_leaf or len(parts) > 1:
       self._children[parts[0]].add_leaf(parts[1:], include_leaf=include_leaf)
 
-  def print(self, path, depth=0, file=sys.stdout):
+  def dfs(self, starting_name, visit_then_descend):
+    """Depth-first visits the tree."""
+    stack = [(starting_name, self)]
+    while stack:
+      current_name, current = stack.pop()
+      if visit_then_descend(current_name, current):
+        for c in reversed(sorted(current.child_names())):
+          stack.append((os.path.join(current_name, c), current.child(c)))
+        
+def print(top, file=sys.stdout):
+  """Prints the tree out."""
+  outfile = file
+  def visitor(current_name, current):
+    nonlocal outfile
+    depth = current_name.count("/") - 1
     prefix = "  " * depth
-    print(f"{prefix}{path} {self._weight}", file=file)
-    for child, child_node in sorted(self._children.items()):
-      child_node.print(os.path.join(path, child), depth=depth + 1, file=file)
+    outfile.write(f"{prefix}{current_name} {current.weight}\n")
+    return True
+  top.dfs("/", visitor)
 
-def dfs(top):
-  stack = [("/", top)]
-  while stack:
-    current_name, current = stack.pop()
-    print(current_name)
-    for c in reversed(current.child_names()):
-      stack.append((os.path.join(current_name, c), current.child(c)))
-
-def pardfs(top, max_weight):
+def partition(top, max_weight):
+  """Partition the tree TOP into subtrees with MAX_WEIGHT per subtree."""
   partitions = []
   current_partition = []
   remaining_space = max_weight
-  stack = [("/", top)]
-  while stack:
-    current_name, current = stack.pop()
+  def visit_then_descend(current_name, current):
+    nonlocal partitions
+    nonlocal current_partition
+    nonlocal remaining_space
     if current.weight <= remaining_space:
       current_partition.append(f"{current_name} {current.weight}")
       remaining_space -= current.weight
@@ -58,22 +73,13 @@ def pardfs(top, max_weight):
         partitions.append(current_partition)
         current_partition = []
         remaining_space = max_weight
-    else:
-      for c in reversed(sorted(current.child_names())):
-        stack.append((os.path.join(current_name, c), current.child(c)))
+      return False  # do not go down the tree
+    return True  # go down the tree.
+  top.dfs("/", visit_then_descend)
   if current_partition:
     partitions.append(current_partition)
   return partitions
-
-def build(linestream, include_leaf=True):
-  top = TreeNode()
-  for line in linestream:
-    if not line.startswith("/"):
-      continue
-    line_parts = line.strip().lstrip("/").split("/")
-    top.add_leaf(line_parts, include_leaf=include_leaf)
-  return top
-
+    
 # Local Variables:
 # python-indent-offset: 2
 # End:
--- treepart_main.py
+++ treepart_main.py
@@ -10,17 +10,17 @@ def main(argv):
   parser = argparse.ArgumentParser(description="tree partition demo.")
   parser.add_argument("--size", metavar="N", type=int, default=100,
                       dest="size", help="Partition target size.")
-  parser.add_argument("--print", dest="print", action=store_true,
-                      "If true, prints the tree.")
+  parser.add_argument("--print", dest="print", action="store_true",
+                      help="If true, prints the tree.")
   parser.add_argument("files", metavar="FILE", nargs="*",
                       help="Files to process.")
   args = parser.parse_args(argv[1:])
   with fileinput.input(args.files) as instream:
     root = treepart.build(instream)
   if args.print:
-    root.print("/")
+    treepart.print(root)
   if args.size > 0:
-    parts = treepart.pardfs(root, args.size)
+    parts = treepart.partition(root, args.size)
     i = 0
     for p in parts:
       print("Partition {}\n\t{}".format(i, "\n\t".join(p)))
